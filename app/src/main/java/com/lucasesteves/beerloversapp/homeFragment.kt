c7ceb6d39cdd8b7219ac403f731ff7fb32bcda46
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-12 18:53:59 -0300

    First Commit

---
+++ app/src/main/java/com/lucasesteves/beerloversapp/homeFragment.kt
@@ -0,0 +1,56 @@
+package com.lucasesteves.beerloversapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.lucasesteves.beerloversapp.R
+import com.lucasesteves.beerloversapp.databinding.FragmentHomeBinding
+
+
+class homeFragment : Fragment() {
+    private var binding: FragmentHomeBinding? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding?.root
+
+
+//        binding?.deslogar?.setOnClickListener{
+//
+//            startActivity(Intent(activity, LoginActivity::class.java))
+//
+//        }
+
+
+
+//        val bundle = Bundle()
+//        with(bundle) {
+//
+//            var user = getString("KEY_USER")
+//            var password = getString("KEY_PASSWORD")
+//
+//        }
+
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
